d1bbfb09f954bf64d928364fbbab1490b91db472
keony1 <keony@edu.univali.br>
2019-11-04 09:04:00 -0300

    Rotas put/delete containers

--- API/src/controllers/ContainerController.js
+++ API/src/controllers/ContainerController.js
@@ -1,4 +1,4 @@
-const Container = require('../models/Container');
+const Container = require("../models/Container");
 
 module.exports = {
   //lista todos os containers
@@ -18,9 +18,49 @@ module.exports = {
       _creator,
       motoristName,
       type,
-      size   
+      size
     });
-    
+
     return res.json(container);
+  },
+
+  async update(req, res) {
+    const { id } = req.params;
+    const { type, size, motoristName } = req.body;
+
+    const container = await Container.findById(id);
+
+    if (!container) {
+      return res.status(400).json({ error: "Container does not exists" });
+    }
+
+    await container.updateOne({
+      $set: {
+        type,
+        size,
+        motoristName
+      }
+    });
+
+    return res.json({
+      id,
+      type,
+      size,
+      motoristName
+    });
+  },
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    const container = await Container.findById(id);
+
+    if (!container) {
+      return res.status(400).json({ error: "Container does not exists" });
+    }
+
+    await container.remove();
+
+    return res.json({ success: "Container has been deleted!" });
   }
-}
+};
--- API/src/routes.js
+++ API/src/routes.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const ContainerController = require('./controllers/ContainerController');
-const UserController = require('./controllers/UserController');
+const express = require("express");
+const ContainerController = require("./controllers/ContainerController");
+const UserController = require("./controllers/UserController");
 const routes = express.Router();
 
 //rotas container
-routes.get('/containers', ContainerController.index);
-routes.post('/containers', ContainerController.store);
+routes.get("/containers", ContainerController.index);
+routes.post("/containers", ContainerController.store);
+routes.put("/containers/:id", ContainerController.update);
+routes.delete("/containers/:id", ContainerController.delete);
 
 //rotas usuário
-routes.post('/login', UserController.signIn);
-routes.post('/user/create', UserController.store);
+routes.post("/login", UserController.signIn);
+routes.post("/user/create", UserController.store);
 
 module.exports = routes;
