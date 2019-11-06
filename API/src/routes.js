63638f0a97e39f3de435cd42291ba0a29ec4c62d
Keony Willian Schröer <keony@edu.univali.br>
2019-11-06 20:10:34 -0300

    Rota indexById retirada do arquivo de rotas da API

--- API/src/routes.js
+++ API/src/routes.js
@@ -5,7 +5,6 @@ const routes = express.Router();
 
 //rotas container
 routes.get("/containers", ContainerController.index);
-routes.get("/containers/:id", ContainerController.indexById);
 routes.post("/containers", ContainerController.store);
 routes.put("/containers/:id", ContainerController.update);
 routes.delete("/containers/:id", ContainerController.delete);
